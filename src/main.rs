66b364e4976851dad570a3ff28785b89a7f248c4
Max Brown <max.brown@ed.ac.uk>
2022-12-07 18:54:57 +0000

    Change serialization to JsonSerializer.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use nu_plugin::{serve_plugin, MsgPackSerializer};
+use nu_plugin::{serve_plugin, JsonSerializer};
 use nu_plugin_bio::Bio;
 
 fn main() {
-    serve_plugin(&mut Bio {}, MsgPackSerializer {})
+    serve_plugin(&mut Bio {}, JsonSerializer {})
 }
